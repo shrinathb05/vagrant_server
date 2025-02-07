5cae530c1024664df1dd824fd4756447e168d41d
Shrinath Bhosale <139325389+shrinathb05@users.noreply.github.com>
2025-02-08 05:03:54 +0530

    Create 3.variable.py
    one more file added

---
+++ scripts/3.variable.py
@@ -0,0 +1,72 @@
+# Variable Assignments
+var1 = "Python"   # STRING
+var2 = 75         # Integer
+var3 = 3.5        # Float
+
+# Printing Variables
+print(var1)
+print(var2)
+print(var3)
+
+# Multiple assignments
+a = b = c = 65
+
+print(a)
+print(b)
+print(c)
+
+# Multiple Values assigned to Multiple Variables
+w, x, y, z = "alpha", "beta", 12, 5.4
+
+# Printing Variables
+print("Variable w value is ", w)
+print("Variable x value is ", x)
+print("Variable y value is ", y)
+print("Variable z value is ", z)
+
+print("Variable w is ", type(w))
+print("Variable x is ", type(x))
+print("Variable y is ", type(y))
+print("Variable z is ", type(z))
+
+# Variables
+num1=123
+str1="alpha"
+
+# list / collection of multiple datatypes enclosed in square brackets[]
+# it's mutable you can edit list
+first_list = [str1, "Devops", 47, num1, 1.5]
+
+# Printing a list
+print(first_list)
+
+# tuple / collection of multiple datatypes enclosed in round brackets ()
+# tuples are immutable you can not edit list
+first_tuple = (str1, "Devops", 47, num1, 1.5)
+
+# Printing a tuple
+print(first_tuple)
+
+# Dictionary / collection of key values pair elements in dictionary are always in key pairs(Key:value) curly brackets {}
+first_dictionary = {"Name":"Shrinath", "Weight":73, "Exercises":["Boxing","Dancing","Jogging"]}
+
+# Printing Dictionary
+print(first_dictionary)
+
+print("Variable first_list is a:", type(first_list))
+print("Variable first_tuple is a:", type(first_tuple))
+print("Variable first_dictionary is a:", type(first_dictionary))
+
+# Boolean
+x = True
+y = False
+
+# Printing Boolean
+print(x)
+print(y)
+
+print("X is a:", type(x))
+print("Y is a:", type(y))
+
+
+
