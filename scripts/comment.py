5638a08cf469f4a38fd20a29b9abc705d5171f0a
Shrinath Bhosale <139325389+shrinathb05@users.noreply.github.com>
2025-02-08 04:30:49 +0530

    Create comment.py
    added a comment file

---
+++ scripts/comment.py
@@ -0,0 +1,27 @@
+#This is a single line comment
+
+"""
+This is a multi line comment
+"""
+
+'''
+This one is also a multi line comment
+
+'''
+
+Skill = "DevOps"
+print("Skill")      # This is a string 
+print('Skill')      # This is a string 
+print(Skill)        # This is a variable
+
+print("""
+This is a paragraph string
+This is the second line of the paragraph
+third line
+""")            # This is a paragraph double quotes
+
+print('''
+This is a paragraph string
+This is the second line of the paragraph
+third line
+''')            # This is a paragraph single quotes
