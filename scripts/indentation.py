0e3c56f2692d7d1addcd1e5679a18d1d2c6c3ce0
Shrinath Bhosale <139325389+shrinathb05@users.noreply.github.com>
2025-02-08 04:23:29 +0530

    Create indentation.py
    new python files added

---
+++ scripts/indentation.py
@@ -0,0 +1,13 @@
+x=2
+print("Learning Indentation")
+print()
+
+if x == 0:
+    print("In th if block")
+    print("Value of x is 0")
+else:
+    print("In the else block")
+    print("value of x is non zero")
+    
+print()
+print("The statement is outof if/else block")
